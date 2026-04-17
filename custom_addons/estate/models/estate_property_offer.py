6c55f1f4e0bd226bc4f65ef772ba817df98f8c10
nahom7874 <biniamn22@gmail.com>
2026-04-18 01:24:22 +0300

    created a estate_property_offer model and linked it to estate_property and partner model

---
+++ custom_addons/estate/models/estate_property_offer.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Real Estate Property Offers"
+    price = fields.Float(string="Offer Price", required=True)
+    status = fields.Selection([
+        ("accepted", "Accepted"),
+        ("refused", "Refused")
+    ], string="Status", default="accepted", required=True)
+    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", required=True)
